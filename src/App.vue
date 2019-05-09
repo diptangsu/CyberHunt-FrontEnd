9c458933e684b5a57a4075bea5f88a110b36a76c
diptangsu <deep.dippu@gmail.com>
2019-05-09 23:01:52 +0530

    Tanumoy performed some magic here

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,69 @@
 <template>
-  <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+  <v-app id="inspire" dark>
+    <v-navigation-drawer v-model="sideNav" clipped left-fixed-navbar app>
+      <v-list dense>
+        <v-list-tile v-for="item in menuItems1" :key="item.title" router :to="item.link">
+          <v-list-tile-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile v-if="userIsAuthenticated" @click="onLogout">
+          <v-list-tile-action>
+            <v-icon>exit_to_app</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Logout</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar app fixed clipped-left>
+      <v-toolbar-side-icon @click.stop="sideNav = !sideNav"></v-toolbar-side-icon>
+      <v-toolbar-title>
+        <router-link to="/" tag="span" style="cursor:pointer">DevMeet</router-link>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
+      <v-toolbar-items class="hidden-xs-only">
+        <v-btn flat v-for="item in menuItems2" :key="item.title" router :to="item.link">
+          <v-icon left>{{ item.icon }}</v-icon>
+          {{ item.title }}
+        </v-btn>
+        <v-btn flat v-if="userIsAuthenticated" @click="onLogout">
+          <v-icon left>exit_to_app</v-icon>Logout
+        </v-btn>
+      </v-toolbar-items>
     </v-toolbar>
-
     <v-content>
-      <HelloWorld/>
+      <router-view></router-view>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  },
-  data () {
+  data() {
     return {
-      //
-    }
+      sideNav: true,
+      menuItems1: [
+        { icon: "face", title: "One", link: "/signup" },
+        { icon: "lock_open", title: "Two", link: "/signin" },
+        { icon: "lock_open", title: "Two", link: "/signin" },
+        { icon: "lock_open", title: "Two", link: "/signin" },
+        { icon: "lock_open", title: "Two", link: "/signin" },
+        { icon: "lock_open", title: "Two", link: "/signin" },
+        { icon: "lock_open", title: "Three", link: "/signin" },
+        { icon: "lock_open", title: "Three", link: "/signin" },
+        { icon: "lock_open", title: "Three", link: "/signin" },
+        { icon: "lock_open", title: "Three", link: "/signin" }
+      ],
+      menuItems2: [
+        { icon: "face", title: "Sign up", link: "/signup" },
+        { icon: "lock_open", title: "Sign in", link: "/signin" }
+      ]
+    };
   }
-}
+};
 </script>
--- src/main.js
+++ src/main.js
@@ -5,7 +5,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import axios from 'axios'
+
 Vue.config.productionTip = false
+
+axios.defaults.baseURL = 'http://127.0.0.1:4000'
 
 new Vue({
   router,
